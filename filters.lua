c4d481d8c7e35324b85fce27fe26092b3aa2ead3
Jon Craton <jncraton@gmail.com>
2026-03-13 12:29:57 -0400

    Add filters.lua

---
+++ filters.lua
@@ -0,0 +1,66 @@
+function Image (el)
+  return {}
+end
+
+function Figure (el)
+  return {}
+end
+
+function RawBlock(el)
+  return {}
+end
+
+function RawInline(el)
+  return {}
+end
+
+function Cite(el)
+  return {}
+end
+
+function Note(el)
+  return {}
+end
+
+function Span (el)
+  if #el.content == 0 then
+    return {}
+  end
+end
+
+function Div (el)
+  if #el.content == 0 then
+    return {}
+  end
+
+  return el.content
+end
+
+function remove_attrs(el)
+  if el.attr then
+    el.attr = {}
+  end
+  return el
+end
+
+function Header(el)
+  return remove_attrs(el)
+end
+
+function Code(el)
+  return remove_attrs(el)
+end
+
+function CodeBlock(el)
+  return remove_attrs(el)
+end
+
+function Para(el)
+  -- Remove trailing period on paragraphs
+  local inls = el.content
+  if #inls == 0 then
+    return nil
+  end
+
+  return pandoc.Para(inls)
+end
